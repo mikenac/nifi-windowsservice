5d74aedbf1a5698324ec9324cfd1c9f686f87fd3
Mike Nacey <mnacey@gmail.com>
2017-02-07 11:56:32 -0500

    Initial Checkin

---
+++ resources/install_service.bat
@@ -0,0 +1,63 @@
+@echo off
+
+set NIFI_VERSION=1.1.1
+
+call nifi-env.bat
+set LIB_DIR=%NIFI_ROOT%\lib\bootstrap
+set CONF_DIR=%NIFI_ROOT%\conf
+
+set BOOTSTRAP_CONF_FILE=%CONF_DIR%\bootstrap.conf
+set SRV_BIN=%cd%\nifi-service.exe
+set SVC_NAME=nifi-service
+set SVC_DISPLAY="Apache NiFi"
+set SVC_DESCRIPTION="Apache NiFi Data Integration Engine"
+set JVM=auto
+set PR_JVMMS=128
+set PR_JVMMX=512
+set PR_JVMSS=4000
+set START_MODE=jvm
+set STOP_MODE=jvm
+set STOP_TIMEOUT=10
+set STARTUP=auto
+set JAVA_ARGS=-Dorg.apache.nifi.bootstrap.config.log.dir="%NIFI_LOG_DIR%";-Dorg.apache.nifi.bootstrap.config.pid.dir="%NIFI_PID_DIR%";-Dorg.apache.nifi.bootstrap.config.file="%BOOTSTRAP_CONF_FILE%"
+SET JAVA_PARAMS=-cp %CONF_DIR%;%LIB_DIR%\* -Xms12m -Xmx24m %JAVA_ARGS% org.apache.nifi.bootstrap.RunNiFi
+set START_CLASS=org.apache.nifi.bootstrap.WindowsService
+set START_METHOD=start
+set STOP_CLASS=org.apache.nifi.bootstrap.WindowsService
+set STOP_METHOD=stop
+REM START_PARAMS=-verbose
+set START_PARAMS=
+REM STOP_PARAMS=-verbose
+set STOP_PARAMS=
+set CLASS_PATH=lib\bootstrap\nifi-windowsservice-%NIFI_VERSION%.jar;%CONF_DIR%;%LIB_DIR%\*;
+set LOG_PATH="%NIFI_ROOT%\logs"
+set LOG_PREFIX=nifi-service.log
+
+"%SRV_BIN%" //IS//%SVC_NAME% ^
+--DisplayName=%SVC_DISPLAY% ^
+--Description=%SVC_DESCRIPTION% ^
+--Install="%SRV_BIN%" ^
+--Jvm="%JVM%" ^
+--JvmMs="%PR_JVMMS%" ^
+--JvmMx="%PR_JVMMX%" ^
+--JvmSs="%PR_JVMSS%" ^
+--StartMode="%START_MODE%" ^
+--StopMode="%STOP_MODE%" ^
+--Startup="%STARTUP%" ^
+--StartClass="%START_CLASS%" ^
+--StopClass="%STOP_CLASS%" ^
+--StartParams="%START_PARAMS%" ^
+--StopParams="%STOP_PARAMS%" ^
+--StartMethod="%START_METHOD%" ^
+--StopMethod="%STOP_METHOD%" ^
+--StopTimeout="%STOP_TIMEOUT%" ^
+--Classpath="%CLASS_PATH%" ^
+--JvmOptions="%JAVA_ARGS%" ^
+--LogLevel=ERROR ^
+--LogPath="%LOG_PATH%" ^
+--LogPrefix="%LOG_PREFIX%" ^
+--StdOutput=auto ^
+--StdError=auto
+
+REM Start the service after installation
+"%SRV_BIN%" //ES//%SVC_NAME%
---
+++ src/main/java/org/apache/nifi/bootstrap/WindowsService.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.bootstrap;
+
+import java.io.IOException;
+import java.io.File;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *<p>Windows service wrapper for NiFi bootstrapping</p>
+ * @author mike-nacey
+ */
+public class WindowsService {
+    public static final String DEFAULT_CONFIG_FILE = "./conf/bootstrap.conf";
+   // used for logging all info. These by default will be written to the log file
+    private static final Logger commandLogger = LoggerFactory.getLogger("org.apache.nifi.bootstrap.Command");
+    private static final File bootstrapConfigFile = getBootstrapConfFile();
+    private static RunNiFi bootstrap;
+
+    public static void start (String[] args) throws IOException, InterruptedException{
+        boolean verbose = isVerbose(args);
+   
+        commandLogger.info("WindowsService-Starting NiFi");
+        bootstrap = new RunNiFi(getBootstrapConfFile(), verbose);
+        bootstrap.start();
+        commandLogger.info("WindowsService-NiFi run method exiting.");
+    }
+    
+    public static void stop(String[] args) throws IOException, InterruptedException {
+        commandLogger.info("WindowsService-Stopping NiFi");
+        // disable this or NiFi will try to restart itself
+        bootstrap.setAutoRestartNiFi(false);
+        bootstrap.stop();
+        commandLogger.info("WindowsService-NiFi stop process completed.");
+    }
+    
+    private static boolean isVerbose (String[] args) {
+        if (args.length == 1) {
+            if (args[0].equalsIgnoreCase("-verbose")) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    // Copied from RunNiFi so as not to require modification
+    private static File getBootstrapConfFile() {
+        String configFilename = System.getProperty("org.apache.nifi.bootstrap.config.file");
+
+        if (configFilename == null) {
+            final String nifiHome = System.getenv("NIFI_HOME");
+            if (nifiHome != null) {
+                final File nifiHomeFile = new File(nifiHome.trim());
+                final File configFile = new File(nifiHomeFile, DEFAULT_CONFIG_FILE);
+                configFilename = configFile.getAbsolutePath();
+            }
+        }
+
+        if (configFilename == null) {
+            configFilename = DEFAULT_CONFIG_FILE;
+        }
+
+        final File configFile = new File(configFilename);
+        return configFile;
+    }
+}
+
